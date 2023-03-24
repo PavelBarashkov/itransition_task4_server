02f9d7b1e3a47fded2db6f909e9db2eebd020ae8
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-03-24 23:36:26 +0400

    test1

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express');
-const sequelize = require('./db');
+const sequelizer = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
-const router = require('./routes/index');
-const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const router = require('./routes/index')
+const errorHandler = require('./milddleware/ErrorHandlingMiddleware');
+
+const PORT = process.env.PORT
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 app.use('/api', router);
+
 app.use(errorHandler);
 
-const start = async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-  } catch (e) {
-    console.log(e);
-  }
-};
 
-start();
+
+const start = async () => {
+    try {
+        await sequelizer.authenticate(); 
+        await sequelizer.sync(); 
+        app.listen(PORT, () => console.log(`Порт успешно изменен на ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start() 
